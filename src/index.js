68be2500bfa846e7667bfccfc4bcca578312f939
Berrnuda <162148781+Berrnuda@users.noreply.github.com>
2024-09-08 17:56:39 +0900

    refactor: og image 수정

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import { BrowserRouter as Router } from "react-router-dom";
 
+const siteUrl = process.env.REACT_APP_SITE_URL;
+
+const setMetaTags = () => {
+    document
+        .querySelector('meta[property="og:image"]')
+        .setAttribute("content", `${siteUrl}/favicon.ico`);
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+setMetaTags();
+
 root.render(
     <Router>
         <App />
